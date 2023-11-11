fff932bae6ab8d4c42ad3c480b7de1dee4e39e93
Ravshan007 <ravshanaliyev9002@gmail.com>
2023-11-11 14:03:36 +0500

    Everything is uploaded

--- src/index.js
+++ src/index.js
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-// import { AppProvider } from './ContextProvider';
-// import reducer, { initialState } from './reducer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <AppProvider reducer={reducer} initialState={initialState}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
-    // </AppProvider> 
 );
